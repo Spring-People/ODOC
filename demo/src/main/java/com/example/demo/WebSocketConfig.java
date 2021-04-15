62dab8ab3d986f2e0ba73e309bd92386a69e97b2
JDhyeok <jdhyeok97@gmail.com>
2021-04-15 22:25:57 +0900

    chore: add web socket configuration

--- demo/src/main/java/com/example/demo/WebSocketConfig.java
+++ demo/src/main/java/com/example/demo/WebSocketConfig.java
@@ -15,14 +15,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     /*
      웹 소켓 구현을 위한 configuration
      */
-//    @Override
-//    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        registry.enableSimpleBroker("/sub"); // 클라이언트 에게 보낼때 api prefix
-//        registry.setApplicationDestinationPrefixes("/pub"); //  클라이언트에서 메시지 송신 시 붙일 api prefix
-//    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic"); // 클라이언트 에게 보낼때 api prefix
+        registry.setApplicationDestinationPrefixes("/app"); //  클라이언트에서 메시지 송신 시 붙일 api prefix
+    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").setAllowedOrigins("*"); // 최초 소켓 연결시 endpoint url
+        registry.addEndpoint("/websocket").withSockJS(); // 최초 소켓 연결시 endpoint url
     }
 }
